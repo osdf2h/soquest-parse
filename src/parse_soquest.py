6a66950d8f67806da2c7719739ef105b4761f9f9
Max Ryabov <maxkekqwe@gmail.com>
2023-07-23 18:28:01 +0500

    feat: add new hours left column

--- src/parse_soquest.py
+++ src/parse_soquest.py
@@ -2,6 +2,8 @@ import logging
 import datetime
 import json
 import math
+import time
+
 from tqdm import tqdm
 
 import requests
@@ -28,18 +30,20 @@ class SoQuest:
         self.upload_data = []
 
     def parse_data(self):
-        total_count = self._test_response()
+        logging.info('Made by https://t.me/soquest_everyday')
+        time.sleep(3)
+        total_count = self.__get_campaigns_count()
         if total_count > 0:
             total_pages = math.ceil(total_count / PAGE_SIZE)
             logging.info(f'Got {total_count} campaigns. Total pages: {total_pages}')
             for page in tqdm(range(1, total_pages+1)):
-                self.data.extend(self._get_data_per_page(page))
-            self._process_data()
+                self.data.extend(self.__get_data_per_page(page))
+            self.__process_data()
             self.__dump_xlsx()
         else:
             logging.info('No active unfinished campaigns found')
 
-    def _test_response(self) -> int:
+    def __get_campaigns_count(self) -> int:
         logging.info(f'Start parsing SoQuest campaigns for address: {self.address}')
         params = {
             'campaign_type': 'all',
@@ -58,7 +62,7 @@ class SoQuest:
         else:
             return 0
 
-    def _get_data_per_page(self, page: int) -> dict | None:
+    def __get_data_per_page(self, page: int) -> dict | None:
         params = {
             'campaign_type': 'all',
             'reward_type': 'all',
@@ -77,17 +81,28 @@ class SoQuest:
             logging.error(f'Error with API response. Status code: {response.status_code}')
             return None
 
-    def _process_data(self):
+    def __process_data(self):
         for data in self.data:
             gems_count = 20 if data.get('is_verify') and data.get('is_recommend')\
                 else (10 if data.get('is_verify')
                       else (1))
+
+            end_timestamp = data.get('end_time')
+            if end_timestamp:
+                current_time = datetime.datetime.now()
+                target_time = datetime.datetime.fromtimestamp(end_timestamp)
+                time_difference = target_time - current_time
+                hours_left = round(time_difference.total_seconds() / 3600, 2)
+            else:
+                hours_left = 'Нет ограничения'
+
             self.upload_data.append(
                 {
                     'Кол-во гемов': gems_count,
                     'Ссылка': data.get('url'),
                     'Кол-во заданий': data.get('task_count'),
-                    'Тип призов': ', '.join(data.get("prize_types"))
+                    'Тип призов': ', '.join(data.get("prize_types")),
+                    'Осталось времени (ч.)': hours_left
                 }
             )
 
@@ -100,12 +115,3 @@ class SoQuest:
         df = df.sort_values(by='Кол-во гемов', ascending=False)
         cur_datetime = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
         df.to_excel(BASE_DIR / 'assets' / f'result_{cur_datetime}.xlsx', index=False)
-
-
-def test():
-    soquest = SoQuest('zxcqwe')
-    soquest.parse_data()
-
-
-if __name__ == '__main__':
-    test()
